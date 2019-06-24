459c50b46784c40a7b0aa36a631925df0a0e78e3
Héctor Sabillón <h.hsabillon@gmail.com>
2019-06-24 12:12:11 -0600

    Correción del campo a desplegar en el ListBox de Zoológicos.

--- 11 - Administrador de Zoologico/MainWindow.xaml.cs
+++ 11 - Administrador de Zoologico/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+// Agregando los namespaces necesarios para SQL Server
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace _11___Administrador_de_Zoologico
 {
@@ -20,11 +24,52 @@ namespace _11___Administrador_de_Zoologico
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Variable miembro
+        SqlConnection sqlconnection;
+
+        // Constructor
         public MainWindow()
         {
             InitializeComponent();
 
             // ZoologicoConnectionString
+            string connectionString = ConfigurationManager.ConnectionStrings["_11___Administrador_de_Zoologico.Properties.Settings.ZoologicoConnectionString"].ConnectionString;
+            sqlconnection = new SqlConnection(connectionString);
+
+            // Llenar el ListBox de Zoológicos
+            MostrarZoologicos();
+        }
+
+        private void MostrarZoologicos()
+        {
+            try
+            {
+                // El query ha realizar en la BD
+                string query = "SELECT * FROM Zoo.Zoologico";
+
+                // SqlDataAdapter es una interfaz entre las tablas y los objetos utilizables en C#
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlconnection);
+
+                using (sqlDataAdapter)
+                {
+                    // Objecto en C# que refleja una tabla de una BD
+                    DataTable tablaZoologico = new DataTable();
+
+                    // Llenar el objeto de tipo DataTable
+                    sqlDataAdapter.Fill(tablaZoologico);
+
+                    // ¿Cuál información de la tabla en el DataTable debería se desplegada en nuestro ListBox?
+                    lbZoologicos.DisplayMemberPath = "ciudad";
+                    // ¿Qué valor debe ser entregado cuando un elemento de nuestro ListBox es seleccionado?
+                    lbZoologicos.SelectedValuePath = "id";
+                    // ¿Quién es la referencia de los datos para el ListBox (popular?
+                    lbZoologicos.ItemsSource = tablaZoologico.DefaultView;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
         }
     }
 }
